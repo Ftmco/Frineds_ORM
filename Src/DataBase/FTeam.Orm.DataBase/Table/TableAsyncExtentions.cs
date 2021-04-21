ece3ba9acd6d1a7ceb4f306e141fb927dd594025
amir hossein <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-04-21 23:01:27 +0430

    insert table services

---
+++ Src/DataBase/FTeam.Orm.DataBase/Table/TableAsyncExtentions.cs
@@ -0,0 +1,36 @@
+﻿using FTeam.Orm.DataBase.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace FTeam.Orm.DataBase.Table
+{
+    public static class TableAsyncExtentions
+    {
+        public static Task<DataTable> TableAsync(this ConnectionStringModel connectionString,string tableName)
+        {
+            throw new Exception();
+        } 
+
+        public static Task<IEnumerable<TEntity>> GetAllAasync<TEntity>(this DataTable dataTable)
+        {
+            throw new Exception();
+        }
+
+        public static Task<IEnumerable<TEntity>> GetAllAasync<TEntity>(this DataTable dataTable,string query)
+        {
+            throw new Exception();
+        }
+
+        public static Task<TEntity> GetAsync<TEntity>(this DataTable dataTable)
+        {
+            throw new Exception();
+        }
+
+        public static Task<TEntity> GetAsync<TEntity>(this DataTable dataTable,string query)
+        {
+            throw new Exception();
+        }
+    }
+}
