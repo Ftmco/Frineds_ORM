634a682266e43c74d4aca36ce028a8526b5bbdbf
amir hossein <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-04-21 20:07:43 +0430

    update cosmos connection

---
+++ Src/Cosmos/FTeam.Orm.Cosmos/ConnectionBase/ConnectionBase.cs
@@ -0,0 +1,55 @@
+﻿using FTeam.Orm.Results.Connection;
+using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace FTeam.Orm.Cosmos.ConnectionBase
+{
+    public class ConnectionBase : IConnectionBase
+    {
+        private readonly SqlConnection _connection;
+
+        public async Task<CloseConnectionResult> CloseConnectionAsync(string connectionString)
+            => await Task.Run(async () =>
+            {
+                try
+                {
+                    _connection.ConnectionString = connectionString;
+                    await _connection.CloseAsync();
+                    return CloseConnectionResult.Success;
+                }
+                catch (SqlException)
+                {
+                    return CloseConnectionResult.SqlException;
+                }
+                catch (Exception)
+                {
+                    return CloseConnectionResult.Exception;
+                }
+            });
+
+        public async Task<OpenConnectionResult> OpenConnectionAsync(string connectionString)
+            => await Task.Run(async () =>
+            {
+                try
+                {
+                    _connection.ConnectionString = connectionString;
+                    await _connection.OpenAsync();
+                    return OpenConnectionResult.Success;
+                }
+                catch (InvalidOperationException)
+                {
+                    return OpenConnectionResult.InvalidOperationException;
+                }
+                catch (SqlException)
+                {
+                    return OpenConnectionResult.SqlException;
+                }
+                catch (Exception)
+                {
+                    return OpenConnectionResult.Exception;
+                }
+            });
+
+    }
+}
---
+++ Src/Cosmos/FTeam.Orm.Cosmos/ConnectionBase/IConnectionBase.cs
@@ -0,0 +1,12 @@
+﻿using FTeam.Orm.Results.Connection;
+using System.Threading.Tasks;
+
+namespace FTeam.Orm.Cosmos.ConnectionBase
+{
+    public interface IConnectionBase
+    {
+        Task<OpenConnectionResult> OpenConnectionAsync(string connectionString);
+
+        Task<CloseConnectionResult> CloseConnectionAsync(string connectionString);
+    }
+}
