cbd82b380bc6ba163bf71427d763e38965bc645f
amir hossein <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-04-22 20:25:42 +0430

    run tests for new functions

--- Src/Test/Cosmos/FTeam.Orm.Cosmos.Test/Query/QueryBaseTest.cs
+++ Src/Test/Cosmos/FTeam.Orm.Cosmos.Test/Query/QueryBaseTest.cs
@@ -1,6 +1,7 @@
 ﻿using FTeam.Orm.Cosmos.QueryBase;
 using FTeam.Orm.Results.QueryBase;
 using NUnit.Framework;
+using System.Diagnostics;
 
 namespace FTeam.Orm.Cosmos.Test.Query
 {
@@ -17,25 +18,53 @@ namespace FTeam.Orm.Cosmos.Test.Query
         }
 
         [Test]
-        public void QueryTest()
+        public void VoidQueryTest()
         {
-            VoidQueryStatus result = _queryBase.RunVoidQueryAsync(connectionString,"Select * from Users").Result;
+            QueryStatus result = _queryBase.RunVoidQueryAsync(connectionString, "Select * from Users").Result;
 
             switch (result)
             {
-                case VoidQueryStatus.Success:
+                case QueryStatus.Success:
                     Assert.Pass("Success");
                     break;
-                case VoidQueryStatus.Exception:
+                case QueryStatus.Exception:
                     Assert.Fail("Excetpion");
                     break;
-                case VoidQueryStatus.InvalidOperationException:
+                case QueryStatus.InvalidOperationException:
                     Assert.Fail("InvalidOperationException");
                     break;
-                case VoidQueryStatus.SqlException:
+                case QueryStatus.SqlException:
                     Assert.Fail("SqlException");
                     break;
-                case VoidQueryStatus.DbException:
+                case QueryStatus.DbException:
+                    Assert.Fail("DbException");
+                    break;
+                default:
+                    Assert.Fail("Excetpion");
+                    break;
+            }
+        }
+
+        [Test]
+        public void NoneVoidQueryTest()
+        {
+            RunQueryResult result = _queryBase.RunQueryAsync(connectionString, "SELECT * FROM USERS").Result;
+
+            switch (result.QueryStatus)
+            {
+                case QueryStatus.Success:
+                    Assert.Pass($"Success {result.DataTable.Rows.Count}");
+                    break;
+                case QueryStatus.Exception:
+                    Assert.Fail("Excetpion");
+                    break;
+                case QueryStatus.InvalidOperationException:
+                    Assert.Fail("InvalidOperationException");
+                    break;
+                case QueryStatus.SqlException:
+                    Assert.Fail("SqlException");
+                    break;
+                case QueryStatus.DbException:
                     Assert.Fail("DbException");
                     break;
                 default:
