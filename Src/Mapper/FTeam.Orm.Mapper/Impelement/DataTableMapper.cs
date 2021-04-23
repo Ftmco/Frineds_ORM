a33fe82ae903089429cab0e4029aa3d0186f3139
amir hossein <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-04-23 15:29:38 +0430

    insert data table mapper

---
+++ Src/Mapper/FTeam.Orm.Mapper/Impelement/DataTableMapper.cs
@@ -0,0 +1,43 @@
+﻿using FTeam.Orm.Mapper.Rules;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace FTeam.Orm.Mapper.Impelement
+{
+    public class DataTableMapper : IDataTableMapper
+    {
+        public IEnumerable<T> Map<T>(DataTable dataTable)
+        {
+            IEnumerable<string> columnNames = dataTable.Columns.Cast<DataColumn>().Select(c => c.ColumnName.ToLower());
+
+            PropertyInfo[] properties = typeof(T).GetProperties();
+
+            return dataTable.AsEnumerable().Select(row =>
+            {
+                T objT = Activator.CreateInstance<T>();
+                foreach (var item in properties)
+                {
+                    if (columnNames.Contains(item.Name.ToLower()))
+                    {
+                        try
+                        {
+                            item.SetValue(objT, row[item.Name]);
+                        }
+                        catch (Exception)
+                        {
+
+                        }
+                    }
+                }
+                return objT;
+            }).ToList();
+        }
+
+        public async Task<IEnumerable<T>> MapAsync<T>(DataTable dataTable)
+            => await Task.Run(() => Map<T>(dataTable));
+    }
+}
--- Src/Orm/FTeam.Orm/Impelements/Form.cs
+++ Src/Orm/FTeam.Orm/Impelements/Form.cs
@@ -10,27 +10,5 @@ namespace FTeam.Orm.Impelements
     public class Form : IForm
     {
 
-        public OpenConnectionResult Connect(DbConnectionInfo dbConnectionInfo)
-        {
-
-        }
-
-        public Task<OpenConnectionResult> ConnectAsync(DbConnectionInfo dbConnectionInfo)
-        {
-            throw new NotImplementedException();
-        }
-
-        private string CreateConnectionString(DbConnectionInfo dbConnectionInfo)
-        {
-            string connectionString = $"Server={dbConnectionInfo.Server};Initial Catalog={dbConnectionInfo.DataBaseName}";
-
-            connectionString = dbConnectionInfo.Authentication switch
-            {
-                Authentication.SqlServerAuthentication => $"User Id={dbConnectionInfo.UserId};Password={dbConnectionInfo.Password}",
-                _=> $""
-            };
-
-            return connectionString;
-        }
     }
 }
