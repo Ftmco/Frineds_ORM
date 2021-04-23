545b5a3081176424417f70ca8f88fca8f31a9624
amir hossein <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-04-23 13:59:51 +0430

    update

---
+++ Src/Orm/FTeam.Orm/Impelements/Form.cs
@@ -0,0 +1,36 @@
+﻿using FTeam.DependencyController.Kernel;
+using FTeam.Orm.DataBase.Models;
+using FTeam.Orm.Results.Connection;
+using FTeam.Orm.Rules;
+using System;
+using System.Threading.Tasks;
+
+namespace FTeam.Orm.Impelements
+{
+    public class Form : IForm
+    {
+
+        public OpenConnectionResult Connect(DbConnectionInfo dbConnectionInfo)
+        {
+
+        }
+
+        public Task<OpenConnectionResult> ConnectAsync(DbConnectionInfo dbConnectionInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string CreateConnectionString(DbConnectionInfo dbConnectionInfo)
+        {
+            string connectionString = $"Server={dbConnectionInfo.Server};Initial Catalog={dbConnectionInfo.DataBaseName}";
+
+            connectionString = dbConnectionInfo.Authentication switch
+            {
+                Authentication.SqlServerAuthentication => $"User Id={dbConnectionInfo.UserId};Password={dbConnectionInfo.Password}",
+                _=> $""
+            };
+
+            return connectionString;
+        }
+    }
+}
