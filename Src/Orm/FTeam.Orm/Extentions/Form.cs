11fe4c3a0fb281a57b11f0026dc8be585b69e23c
amir hossein <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-04-24 22:32:45 +0430

    insert crud extensions

--- Src/Orm/FTeam.Orm/Extentions/Form.cs
+++ Src/Orm/FTeam.Orm/Extentions/Form.cs
@@ -1,5 +1,6 @@
-﻿using FTeam.Orm.DataBase.Extentions;
-using FTeam.Orm.Models;
+﻿using FTeam.Orm.Models;
+using FTeam.Orm.Results.QueryBase;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace FTeam.Orm.Extentions
@@ -14,5 +15,74 @@ namespace FTeam.Orm.Extentions
         public static async Task<TableInfoResult> TableAsync(this DbConnectionInfo dbConnectionInfo, string tableName)
             => await Task.Run(() => dbConnectionInfo.Table(tableName));
 
+        public static async Task<IEnumerable<T>> GetAllAsync<T>(this TableInfoResult tableInfo)
+        {
+
+        }
+
+        public static IEnumerable<T> GetAll<T>(this TableInfoResult tableInfo)
+        {
+
+        }
+
+        public static async Task<IEnumerable<T>> GetAllAsync<T>(this TableInfoResult tableInfo, string query)
+        {
+
+        }
+
+        public static IEnumerable<T> GetAll<T>(this TableInfoResult tableInfo, string query)
+        {
+
+        }
+
+        public static async Task<T> GetAsync<T>(this TableInfoResult tableInfo, string query)
+        {
+
+        }
+
+        public static T Get<T>(this TableInfoResult tableInfo, string query)
+        {
+
+        }
+
+        public static async Task<T> GetAsync<T>(this TableInfoResult tableInfo)
+        {
+
+        }
+
+        public static T Get<T>(this TableInfoResult tableInfo)
+        {
+
+        }
+
+        public static QueryStatus Insert<T>(this TableInfoResult tableInfo,T instance)
+        {
+
+        }
+
+        public static QueryStatus Update<T>(this TableInfoResult tableInfo, T instance)
+        {
+
+        }
+
+        public static QueryStatus Delete<T>(this TableInfoResult tableInfo,T instance)
+        {
+
+        }
+
+        public static Task<QueryStatus> InsertAsync<T>(this TableInfoResult tableInfo, T instance)
+        {
+
+        }
+
+        public static Task<QueryStatus> UpdateAsync<T>(this TableInfoResult tableInfo, T instance)
+        {
+
+        }
+
+        public static Task<QueryStatus> DeleteAsync<T>(this TableInfoResult tableInfo, T instance)
+        {
+
+        }
     }
 }
