18cf5ce9781dc852119fd4d9d8c85ffc5ed46a5c
amir hossein <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-04-28 22:26:51 +0430

    insert crud rules

---
+++ Src/DataBase/FTeam.Orm.DataBase/Tables/Rules/ITableInsertRules.cs
@@ -0,0 +1,13 @@
+﻿using FTeam.Orm.Models;
+using FTeam.Orm.Results.QueryBase;
+using System.Threading.Tasks;
+
+namespace FTeam.Orm.DataBase.Tables
+{
+    public interface ITableInsertRules
+    {
+        Task<QueryStatus> InsertAsync<T>(TableInfoResult tableInfo, T instance);
+
+        QueryStatus Insert<T>(TableInfoResult tableInfo, T instance);
+    }
+}
