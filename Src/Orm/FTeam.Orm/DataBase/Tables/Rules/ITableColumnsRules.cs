5eaeea1900cd36464163369b2fa2bed88136e4e9
amir hossein <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-05-02 19:45:16 +0430

    Change Table Primary Key Services

--- Src/Orm/FTeam.Orm/DataBase/Tables/Rules/ITableColumnsRules.cs
+++ Src/Orm/FTeam.Orm/DataBase/Tables/Rules/ITableColumnsRules.cs
@@ -6,16 +6,20 @@ namespace FTeam.Orm.DataBase.Tables
 {
     public interface ITableColumnsRules
     {
-        Task<PrimaryKey> TryGetTablePrimaryKeyAsync(DbConnectionInfo dbConnectionInfo, TableInfoResult tableInfoResult);
-        Task<PrimaryKey> GetTablePrimaryKeyAsync(DbConnectionInfo dbConnectionInfo, TableInfoResult tableInfoResult);
+        Task<PrimaryKey> TryGetTablePrimaryKeyAsync<T>();
 
-        PrimaryKey TryGetTablePrimaryKey(DbConnectionInfo dbConnectionInfo, TableInfoResult tableInfoResult);
-        PrimaryKey GetTablePrimaryKey(DbConnectionInfo dbConnectionInfo, TableInfoResult tableInfoResult);
+        Task<PrimaryKey> GetTablePrimaryKeyAsync<T>();
+
+        PrimaryKey TryGetTablePrimaryKey<T>();
+
+        PrimaryKey GetTablePrimaryKey<T>();
 
         IEnumerable<TableColumns> TryGetTableColumns(string tableName, DbConnectionInfo dbConnectionInfo);
+
         IEnumerable<TableColumns> GetTableColumns(string tableName, DbConnectionInfo dbConnectionInfo);
 
         Task<IEnumerable<TableColumns>> TryGetTableColumnsAsync(string tableName, DbConnectionInfo dbConnectionInfo);
+
         Task<IEnumerable<TableColumns>> GetTableColumnsAsync(string tableName, DbConnectionInfo dbConnectionInfo);
     }
 }
