1599a50958580d6dd755d349a6ab51e29e95ec10
amir hossein baderan <amir0hossin0pr@gmail.com>
2021-05-02 16:38:34 +0430

    insert Fkey Attribute

---
+++ Src/Orm/FTeam.Orm/Attributes/FKey.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FTeam.Orm.Attributes
+{
+    [AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+    sealed class FKey : Attribute
+    {
+
+    }
+}
