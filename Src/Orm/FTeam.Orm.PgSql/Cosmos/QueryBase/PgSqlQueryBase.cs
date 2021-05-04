65f5e30303821e5d580b94f6e6663f53bb2efbcf
amir hossein <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-05-04 22:31:41 +0430

    Update PgSqlQueryBase.cs

--- Src/Orm/FTeam.Orm.PgSql/Cosmos/QueryBase/PgSqlQueryBase.cs
+++ Src/Orm/FTeam.Orm.PgSql/Cosmos/QueryBase/PgSqlQueryBase.cs
@@ -1,13 +1,130 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using FTeam.Orm.Models.QueryBase;
+using Npgsql;
+using System;
 using System.Threading.Tasks;
 
 namespace FTeam.Orm.PgSql.Cosmos.QueryBase
 {
     public class PgSqlQueryBase : IPgSqlQueryBase
     {
+        public RunQueryResult RunQuery(string connectionString, string query)
+        {
+            throw new NotImplementedException();
+        }
 
+        public RunQueryResult RunQuery(NpgsqlConnection NpgsqlConnection, string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RunQueryResult> RunQueryAsync(string connectionString, string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RunQueryResult> RunQueryAsync(NpgsqlConnection NpgsqlConnection, string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public QueryStatus RunVoidQuery(string connectionString, string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public QueryStatus RunVoidQuery(NpgsqlConnection NpgsqlConnection, string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public QueryStatus RunVoidQuery(string connectionString, NpgsqlCommand NpgsqlCommand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public QueryStatus RunVoidQuery(NpgsqlConnection NpgsqlConnection, NpgsqlCommand NpgsqlCommand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<QueryStatus> RunVoidQueryAsync(string connectionString, string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<QueryStatus> RunVoidQueryAsync(string connectionString, NpgsqlCommand NpgsqlCommand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<QueryStatus> RunVoidQueryAsync(NpgsqlConnection NpgsqlConnection, NpgsqlCommand NpgsqlCommand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<QueryStatus> RunVoidQueryAsync(NpgsqlConnection NpgsqlConnection, string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RunQueryResult TryRunQuery(string connectionString, string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RunQueryResult TryRunQuery(NpgsqlConnection NpgsqlConnection, string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RunQueryResult> TryRunQueryAsync(string connectionString, string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RunQueryResult> TryRunQueryAsync(NpgsqlConnection NpgsqlConnection, string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public QueryStatus TryRunVoidQuery(string connectionString, string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public QueryStatus TryRunVoidQuery(NpgsqlConnection NpgsqlConnection, string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public QueryStatus TryRunVoidQuery(string connectionString, NpgsqlCommand NpgsqlCommand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public QueryStatus TryRunVoidQuery(NpgsqlConnection NpgsqlConnection, NpgsqlCommand NpgsqlCommand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<QueryStatus> TryRunVoidQueryAsync(string connectionString, string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<QueryStatus> TryRunVoidQueryAsync(string connectionString, NpgsqlCommand NpgsqlCommand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<QueryStatus> TryRunVoidQueryAsync(NpgsqlConnection NpgsqlConnection, NpgsqlCommand NpgsqlCommand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<QueryStatus> TryRunVoidQueryAsync(NpgsqlConnection NpgsqlConnection, string query)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
