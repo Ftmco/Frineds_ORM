ca23937bd8188010d1bb93436b2e076d8b2077f0
amir hossein <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-05-07 14:12:14 +0430

    Change Table Info Results

--- Src/Results/FTeam.Orm.Results/DataBase/Table/PrimaryKey.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FTeam.Orm.Domains.DataBase.Table.SqlServer
-{
-    public record PrimaryKey
-    {
-        public string Column { get; set; }
-
-        public string Type { get; set; }
-    }
-}
---
+++ Src/Results/FTeam.Orm.Results/DataBase/Table/SqlServer/SqlServerTableInfoResult.cs
@@ -0,0 +1,15 @@
+﻿using FTeam.Orm.Domains.Connection.SqlServer;
+using FTeam.Orm.Domains.DataBase.Table.Base;
+using FTeam.Orm.Models.QueryBase;
+
+namespace FTeam.Orm.Models.DataBase.Table.SqlServer
+{
+    public record SqlServerTableInfoResult : ITableInfoResult
+    {
+        public QueryStatus Status { get; set; }
+
+        public TableInfo TableInfo { get; set; }
+
+        public SqlServerDbConnectionInfo SqlServerDbConnectionInfo { get; set; }
+    }
+}
